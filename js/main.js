d4d554909fa62c67f9e3872380ea071ad05ee8e2
alaedin <mona.alaedin1999@gmail.com>
2024-04-09 00:09:04 +0330

    services section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-const hamburgerMenu = document.querySelector(".hamburger");
-const navbarLinks = document.querySelector(".navbar--links");
+// const hamburgerMenu = document.querySelector(".hamburger");
+// const navbarLinks = document.querySelector(".navbar--links");
 
-hamburgerMenu.addEventListener("click", () => {
-  if (hamburgerMenu.classList.contains("active")) {
-    hamburgerMenu.classList.remove("active");
-    navbarLinks.style.display = "none";
-  } else if (!hamburgerMenu.classList.contains("active")) {
-    hamburgerMenu.classList.add("active");
-    navbarLinks.style.display = "flex";
-  }
+// hamburgerMenu.addEventListener("click", () => {
+//   if (hamburgerMenu.classList.contains("active")) {
+//     hamburgerMenu.classList.remove("active");
+//     navbarLinks.style.display = "none";
+//   } else if (!hamburgerMenu.classList.contains("active")) {
+//     hamburgerMenu.classList.add("active");
+//     navbarLinks.style.display = "flex";
+//   }
+// });
+
+// ? services section
+
+const tabs = document.querySelectorAll(".tab");
+const tabPanes = document.querySelectorAll(".tab-pane");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabPanes.forEach((tabPane) => tabPane.classList.remove("active"));
+
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
 });

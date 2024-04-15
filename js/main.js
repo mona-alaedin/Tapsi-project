282f3067fc7eb1ed7c9867a7d52210d94e667c9c
alaedin <mona.alaedin1999@gmail.com>
2024-04-15 20:10:26 +0330

    Project completion

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-// const hamburgerMenu = document.querySelector(".hamburger");
-// const navbarLinks = document.querySelector(".navbar--links");
+const hamburgerMenu = document.querySelector(".hamburger");
+const navbarLinks = document.querySelector(".navbar--links");
 
-// hamburgerMenu.addEventListener("click", () => {
-//   if (hamburgerMenu.classList.contains("active")) {
-//     hamburgerMenu.classList.remove("active");
-//     navbarLinks.style.display = "none";
-//   } else if (!hamburgerMenu.classList.contains("active")) {
-//     hamburgerMenu.classList.add("active");
-//     navbarLinks.style.display = "flex";
-//   }
-// });
+hamburgerMenu.addEventListener("click", () => {
+  if (hamburgerMenu.classList.contains("active")) {
+    hamburgerMenu.classList.remove("active");
+    navbarLinks.style.display = "none";
+  } else if (!hamburgerMenu.classList.contains("active")) {
+    hamburgerMenu.classList.add("active");
+    navbarLinks.style.display = "flex";
+  }
+});
 
 // ? services section
 
